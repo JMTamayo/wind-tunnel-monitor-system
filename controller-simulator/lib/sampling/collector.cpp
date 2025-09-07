2dead96a60882d0a008e33c8b64b9b779907a7a2
JMTamayo <jmtamayog23@gmail.com>
2025-09-07 06:46:53 -0500

    Create specific module for sample struct

--- controller-simulator/lib/sampling/collector.cpp
+++ controller-simulator/lib/sampling/collector.cpp
@@ -5,43 +5,6 @@
 
 namespace sampling {
 
-Sample::Sample(String timestamp, float airFlow, float airFlowCFM,
-               float airPressure, float airTemperature, float airVelocity,
-               float fanFrequency, bool fanState, float turbineAxialForce,
-               float turbineTransverseForce, float turbinePower)
-    : timestamp(timestamp), airFlow(airFlow), airFlowCFM(airFlowCFM),
-      airPressure(airPressure), airTemperature(airTemperature),
-      airVelocity(airVelocity), fanFrequency(fanFrequency), fanState(fanState),
-      turbineAxialForce(turbineAxialForce),
-      turbineTransverseForce(turbineTransverseForce),
-      turbinePower(turbinePower) {}
-
-Sample::~Sample() {}
-
-String Sample::GetTimestamp() const { return this->timestamp; }
-
-float Sample::GetAirFlow() const { return this->airFlow; }
-
-float Sample::GetAirFlowCFM() const { return this->airFlowCFM; }
-
-float Sample::GetAirPressure() const { return this->airPressure; }
-
-float Sample::GetAirTemperature() const { return this->airTemperature; }
-
-float Sample::GetAirVelocity() const { return this->airVelocity; }
-
-float Sample::GetFanFrequency() const { return this->fanFrequency; }
-
-bool Sample::GetFanState() const { return this->fanState; }
-
-float Sample::GetTurbineAxialForce() const { return this->turbineAxialForce; }
-
-float Sample::GetTurbineTransverseForce() const {
-  return this->turbineTransverseForce;
-}
-
-float Sample::GetTurbinePower() const { return this->turbinePower; }
-
 unsigned long Collector::getIntervalMs() const { return this->intervalMs; }
 
 unsigned long Collector::getLastSampleTimeMs() const {
@@ -55,7 +18,9 @@ void Collector::setLastSampleTimeMs(unsigned long lastSampleTimeMs) {
 Collector::Collector(unsigned long intervalMs,
                      services::TimeService *timeService)
     : intervalMs(intervalMs), lastSampleTimeMs(millis()),
-      timeService(timeService) {}
+      timeService(timeService) {
+  this->timeService->Sync();
+}
 
 Collector::~Collector() {}
 
--- controller-simulator/lib/sampling/collector.h
+++ controller-simulator/lib/sampling/collector.h
@@ -2,53 +2,9 @@
 #define COLLECTOR_H
 
 #include "time_.h"
+#include "sample.h"
 
 namespace sampling {
-
-class Sample {
-private:
-  String timestamp;
-  float airFlow;
-  float airFlowCFM;
-  float airPressure;
-  float airTemperature;
-  float airVelocity;
-  float fanFrequency;
-  bool fanState;
-  float turbineAxialForce;
-  float turbineTransverseForce;
-  float turbinePower;
-
-public:
-  Sample(String timestamp, float airFlow, float airFlowCFM, float airPressure,
-         float airTemperature, float airVelocity, float fanFrequency,
-         bool fanState, float turbineAxialForce, float turbineTransverseForce,
-         float turbinePower);
-
-  ~Sample();
-
-  String GetTimestamp() const;
-
-  float GetAirFlow() const;
-
-  float GetAirFlowCFM() const;
-
-  float GetAirPressure() const;
-
-  float GetAirTemperature() const;
-
-  float GetAirVelocity() const;
-
-  float GetFanFrequency() const;
-
-  bool GetFanState() const;
-
-  float GetTurbineAxialForce() const;
-
-  float GetTurbineTransverseForce() const;
-
-  float GetTurbinePower() const;
-};
 
 class Collector {
 private:

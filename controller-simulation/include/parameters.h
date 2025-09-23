2cff39f10137b6272f109d581c22094204e27be8
JMTamayo <jmtamayog23@gmail.com>
2025-09-22 20:27:34 -0500

    Implement parameters.h to handle configuration params for the project

---
+++ controller-simulation/include/parameters.h
@@ -0,0 +1,15 @@
+#define WIFI_MAX_RETRY_TIME_MILLISECONDS 5000
+
+#define MQTT_MAX_RETRY_TIME_MILLISECONDS 5000
+
+#define MQTT_TOPIC_BASE                                                        \
+  "/Thingworx/ThingWorx.WindTunnelControlSystem.Controller"
+
+#define MQTT_TOPIC_AIR_TEMPERATURE "/airTemperature"
+#define MQTT_TOPIC_AIR_RELATIVE_HUMIDITY "/airRelativeHumidity"
+
+#define MQTT_TOPIC_CONTROL_FAN_FREQUENCY "/fanFrequency"
+
+#define MEASURING_INTERVAL_MILLISECONDS 2000
+
+#define DHT22_PIN 34

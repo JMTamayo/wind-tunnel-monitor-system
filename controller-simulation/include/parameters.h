7e67b767d9773e00b4bb6db6a212e295b00bc3c1
JMTamayo <jmtamayog23@gmail.com>
2025-09-28 10:02:47 -0500

    Implement MQTT_TOPIC_WIND_TURBINE_RPM, MQTT_TOPIC_CONTROL_FAN_FREQUENCY_SET_POINT, MQTT_TOPIC_FAN_FREQUENCY_SET_POINT, BERM_E3F_DS30C4_PIN and WIND_TURBINE_PULSE_COUNT_DIVIDER

--- controller-simulation/include/parameters.h
+++ controller-simulation/include/parameters.h
@@ -8,11 +8,18 @@
 #define MQTT_TOPIC_AIR_TEMPERATURE "/airTemperature"
 #define MQTT_TOPIC_AIR_RELATIVE_HUMIDITY "/airRelativeHumidity"
 
-#define MQTT_TOPIC_CONTROL_FAN_FREQUENCY_SET_POINT "/fanFrequencySetPoint"
+#define MQTT_TOPIC_WIND_TURBINE_RPM "/turbineRpm"
+
+#define MQTT_TOPIC_CONTROL_FAN_FREQUENCY_SET_POINT                             \
+  "/fanFrequencySetPointRequest"
+#define MQTT_TOPIC_FAN_FREQUENCY_SET_POINT "/fanFrequencySetPoint"
 #define MQTT_TOPIC_FAN_RPM "/fanRpm"
 
 #define MEASURING_INTERVAL_MILLISECONDS 2000
 
-#define DHT22_PIN 34
+#define FAN_CONTROL_PIN 23
 
-#define FAN_CONTROL_PIN 33
+#define BERM_E3F_DS30C4_PIN 22
+#define WIND_TURBINE_PULSE_COUNT_DIVIDER 3
+
+#define DHT22_PIN 34

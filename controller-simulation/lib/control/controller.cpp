375ce9bddadce2eeb543722bc75ab35673c72225
JMTamayo <jmtamayog23@gmail.com>
2025-09-29 22:29:32 -0500

    Implement the meausure of air flow, air velocity and dynamic pressure in controller

--- controller-simulation/lib/control/controller.cpp
+++ controller-simulation/lib/control/controller.cpp
@@ -2,26 +2,40 @@
 
 namespace control {
 
-Measures::Measures(peripherals::AirProperties airProperties,
-                   peripherals::RotationalFrequency windTurbineProperties,
-                   peripherals::FanProperties fanProperties)
-    : airProperties(airProperties),
-      windTurbineProperties(windTurbineProperties),
-      fanProperties(fanProperties) {}
+Measures::Measures(float airTemperature, float airRelativeHumidity,
+                   float airVelocity, float dynamicPressure, float airFlow,
+                   float airFlowCfm, float airDensity, float windTurbineRpm,
+                   float windTurbinePower, float fanFrequencySetPoint,
+                   float fanRpm)
+    : airTemperature(airTemperature), airRelativeHumidity(airRelativeHumidity),
+      airVelocity(airVelocity), dynamicPressure(dynamicPressure),
+      airFlow(airFlow), airFlowCfm(airFlowCfm), airDensity(airDensity),
+      windTurbineRpm(windTurbineRpm), windTurbinePower(windTurbinePower),
+      fanFrequencySetPoint(fanFrequencySetPoint), fanRpm(fanRpm) {}
 
 Measures::~Measures() {}
 
-peripherals::AirProperties Measures::GetAirProperties() {
-  return this->airProperties;
-}
+float Measures::GetAirTemperature() { return this->airTemperature; }
 
-peripherals::RotationalFrequency Measures::GetWindTurbineProperties() {
-  return this->windTurbineProperties;
-}
+float Measures::GetAirRelativeHumidity() { return this->airRelativeHumidity; }
 
-peripherals::FanProperties Measures::GetFanProperties() {
-  return this->fanProperties;
-}
+float Measures::GetAirVelocity() { return this->airVelocity; }
+
+float Measures::GetDynamicPressure() { return this->dynamicPressure; }
+
+float Measures::GetAirFlow() { return this->airFlow; }
+
+float Measures::GetAirFlowCfm() { return this->airFlowCfm; }
+
+float Measures::GetAirDensity() { return this->airDensity; }
+
+float Measures::GetWindTurbineRpm() { return this->windTurbineRpm; }
+
+float Measures::GetWindTurbinePower() { return this->windTurbinePower; }
+
+float Measures::GetFanFrequencySetPoint() { return this->fanFrequencySetPoint; }
+
+float Measures::GetFanRpm() { return this->fanRpm; }
 
 const unsigned long Controller::getIntervalMs() const {
   return this->intervalMs;
@@ -40,25 +54,50 @@ const unsigned int Controller::getWindTurbinePulseCountDivider() const {
   return this->windTurbinePulseCountDivider;
 }
 
+const float Controller::getCrossSectionArea() const {
+  return this->crossSectionArea;
+}
+
+const float Controller::getAirDensity() const { return this->airDensity; }
+
 peripherals::BermE3fDs30c4 *Controller::getBermE3fDs30c4() {
   return this->bermE3fDs30c4;
 }
 
 peripherals::Dht22 *Controller::getDht22() { return this->dht22; }
 
+peripherals::MPXV7002DP_ADC1115 *Controller::getMpxv7002dp_Adc1115() {
+  return this->mpxv7002dp_Adc1115;
+}
+
 peripherals::Fan *Controller::getFan() { return this->fan; }
 
 Controller::Controller(const unsigned long intervalMs,
                        const unsigned int windTurbinePulseCountDivider,
+                       const float airDensity, const float crossSectionArea,
                        peripherals::BermE3fDs30c4 *bermE3fDs30c4,
-                       peripherals::Dht22 *dht22, peripherals::Fan *fan)
+                       peripherals::Dht22 *dht22,
+                       peripherals::MPXV7002DP_ADC1115 *mpxv7002dp_Adc1115,
+                       peripherals::Fan *fan)
     : intervalMs(intervalMs), lastMeasurementTimeMs(millis()),
       windTurbinePulseCountDivider(windTurbinePulseCountDivider),
-      bermE3fDs30c4(bermE3fDs30c4), dht22(dht22), fan(fan) {}
+      airDensity(airDensity), crossSectionArea(crossSectionArea),
+      bermE3fDs30c4(bermE3fDs30c4), dht22(dht22),
+      mpxv7002dp_Adc1115(mpxv7002dp_Adc1115), fan(fan) {}
+
+float Controller::getWindTurbinePower(float rotationalFrequencyRpm) const {
+  // The following values are for testing purposes. You should delete this when
+  // the real prototype is ready.
+  return 0.0034f * rotationalFrequencyRpm;
+}
 
 Controller::~Controller() {}
 
-void Controller::Begin() { this->getBermE3fDs30c4()->Restart(); }
+void Controller::Begin() {
+  this->getBermE3fDs30c4()->Begin();
+  this->getDht22()->Begin();
+  this->getMpxv7002dp_Adc1115()->Begin();
+}
 
 bool Controller::IsMeasurementTimeReached() {
   return millis() - this->getLastMeasurementTimeMs() >= this->getIntervalMs();
@@ -70,9 +109,26 @@ Measures Controller::Measure() {
   peripherals::RotationalFrequency windTurbineProperties =
       this->getBermE3fDs30c4()->Read(this->getWindTurbinePulseCountDivider());
 
-  this->setLastMeasurementTimeMs(millis());
+  float windTurbinePower = this->getWindTurbinePower(
+      windTurbineProperties.GetRotationalFrequencyRpm());
 
-  return Measures(airProperties, windTurbineProperties, fanProperties);
+  float airDensity = this->getAirDensity();
+  peripherals::DynamicPressure dynamicPressure =
+      this->getMpxv7002dp_Adc1115()->Read(airDensity);
+
+  float airVelocity = dynamicPressure.GetAirVelocity();
+  float airFlow = airVelocity * this->getCrossSectionArea();
+  float airFlowCfm = airFlow * 2118.8799727597;
+
+  Measures measures(
+      airProperties.GetTemperature(), airProperties.GetRelativeHumidity(),
+      airVelocity, dynamicPressure.GetPressure(), airFlow, airFlowCfm,
+      airDensity, windTurbineProperties.GetRotationalFrequencyRpm(),
+      windTurbinePower, fanProperties.GetFrequencySetPoint(),
+      fanProperties.GetRpm());
+
+  this->setLastMeasurementTimeMs(millis());
+  return measures;
 }
 
 void Controller::SetFanFrequency(float frequency) {
--- controller-simulation/lib/control/controller.h
+++ controller-simulation/lib/control/controller.h
@@ -6,27 +6,57 @@
 #include "berm_e3f_ds30c4.h"
 #include "dht22.h"
 #include "fan.h"
+#include "mpxv7002dp_adc1115.h"
 
 namespace control {
 
 class Measures {
 private:
-  peripherals::AirProperties airProperties;
-  peripherals::RotationalFrequency windTurbineProperties;
-  peripherals::FanProperties fanProperties;
+  float airTemperature;
+  float airRelativeHumidity;
+
+  float airVelocity;
+  float airDensity;
+  float dynamicPressure;
+
+  float airFlow;
+  float airFlowCfm;
+
+  float windTurbineRpm;
+  float windTurbinePower;
+
+  float fanFrequencySetPoint;
+  float fanRpm;
 
 public:
-  Measures(peripherals::AirProperties airProperties,
-           peripherals::RotationalFrequency windTurbineProperties,
-           peripherals::FanProperties fanProperties);
+  Measures(float airTemperature, float airRelativeHumidity, float airVelocity,
+           float dynamicPressure, float airFlow, float airFlowCfm,
+           float airDensity, float windTurbineRpm, float windTurbinePower,
+           float fanFrequencySetPoint, float fanRpm);
 
   ~Measures();
 
-  peripherals::AirProperties GetAirProperties();
+  float GetAirTemperature();
 
-  peripherals::RotationalFrequency GetWindTurbineProperties();
+  float GetAirRelativeHumidity();
 
-  peripherals::FanProperties GetFanProperties();
+  float GetAirVelocity();
+
+  float GetAirDensity();
+
+  float GetDynamicPressure();
+
+  float GetAirFlow();
+
+  float GetAirFlowCfm();
+
+  float GetWindTurbineRpm();
+
+  float GetWindTurbinePower();
+
+  float GetFanFrequencySetPoint();
+
+  float GetFanRpm();
 };
 
 class Controller {
@@ -36,8 +66,13 @@ private:
 
   const unsigned int windTurbinePulseCountDivider;
 
+  const float crossSectionArea;
+  const float airDensity;
+
   peripherals::BermE3fDs30c4 *bermE3fDs30c4;
   peripherals::Dht22 *dht22;
+  peripherals::MPXV7002DP_ADC1115 *mpxv7002dp_Adc1115;
+
   peripherals::Fan *fan;
 
   const unsigned long getIntervalMs() const;
@@ -48,17 +83,27 @@ private:
 
   const unsigned int getWindTurbinePulseCountDivider() const;
 
+  const float getCrossSectionArea() const;
+
+  const float getAirDensity() const;
+
   peripherals::BermE3fDs30c4 *getBermE3fDs30c4();
 
   peripherals::Dht22 *getDht22();
 
+  peripherals::MPXV7002DP_ADC1115 *getMpxv7002dp_Adc1115();
+
   peripherals::Fan *getFan();
+
+  float getWindTurbinePower(float rotationalFrequencyRpm) const;
 
 public:
   Controller(unsigned long intervalMs,
-             unsigned int windTurbinePulseCountDivider,
-             peripherals::BermE3fDs30c4 *bermE3fDs30c4,
-             peripherals::Dht22 *dht22, peripherals::Fan *fan);
+             unsigned int windTurbinePulseCountDivider, float airDensity,
+             float crossSectionArea, peripherals::BermE3fDs30c4 *bermE3fDs30c4,
+             peripherals::Dht22 *dht22,
+             peripherals::MPXV7002DP_ADC1115 *mpxv7002dp_Adc1115,
+             peripherals::Fan *fan);
 
   ~Controller();
 

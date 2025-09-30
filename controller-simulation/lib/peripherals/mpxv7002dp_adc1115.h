8f7f1050e873372546f0c883da40367b3964d3a7
JMTamayo <jmtamayog23@gmail.com>
2025-09-29 22:27:31 -0500

    Implement the resources to handle the MPXV7002DP with ADC1115

---
+++ controller-simulation/lib/peripherals/mpxv7002dp_adc1115.h
@@ -0,0 +1,48 @@
+#ifndef MPXV7002DP_ADC1115_H
+#define MPXV7002DP_ADC1115_H
+
+#include <Arduino.h>
+
+namespace peripherals {
+
+class DynamicPressure {
+private:
+  float pressure;
+  float airVelocity;
+
+public:
+  DynamicPressure(float pressure, float airVelocity);
+
+  ~DynamicPressure();
+
+  float GetPressure();
+
+  float GetAirVelocity();
+};
+
+class MPXV7002DP_ADC1115 {
+private:
+  unsigned int channel;
+  float voltageOffset;
+  float voltageSensitivity;
+
+  unsigned int getChannel();
+
+  float getVoltageOffset();
+
+  float getVoltageSensitivity();
+
+public:
+  MPXV7002DP_ADC1115(unsigned int channel, float voltageOffset,
+                     float voltageSensitivity);
+
+  ~MPXV7002DP_ADC1115();
+
+  void Begin();
+
+  DynamicPressure Read(float airDensity);
+};
+
+} // namespace peripherals
+
+#endif // MPXV7002DP_ADC1115_H
